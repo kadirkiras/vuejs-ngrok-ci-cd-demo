90f0bd2408ee4a8a9506c3395ac55e1f668ca07f
kadirkiras <kadirkiras@pm.me>
2024-12-10 23:13:31 +0300

    Update echo endpoint response message to "Hello From Echov2"

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/echo", (req, res) => {
-  res.json({ message: "Hello From Echo" });
+  res.json({ message: "Hello From Echov2" });
 });
 
 app.listen(port, () => {
